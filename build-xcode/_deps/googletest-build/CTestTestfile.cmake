361070a6cfada70c990881c9d5f84e3a4a6c3805
yanbing.xu <605224031@qq.com>
2026-01-05 14:45:44 +0800

    SAVE

--- build-xcode/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yanbing.xu/Desktop/RTR_Metal/build-xcode/_deps/googletest-src
-# Build directory: /Users/yanbing.xu/Desktop/RTR_Metal/build-xcode/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
